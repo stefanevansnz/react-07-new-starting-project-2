6203bcd8faa30ef603ef26cca00912b4095747f6
Stefan Evans <stefan.evans@consegna.cloud>
2022-04-18 18:10:50 +1200

    added links

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return <div>
+    <MainNavigation />
     <Switch>
       <Route path='/' exact>
         <AllMeetupsPage />
       </Route>
-      <Route path='/new-meetups'>
+      <Route path='/new-meetup'>
         <NewMeetupsPage />
       </Route>
       <Route path='/favourites'>
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function MainNavigation() {
+    return <header>
+        <div>React Meetups</div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/new-meetup'>Add New Meetup</Link>
+                </li>
+                <li>
+                    <Link to='/favourites'>My Favourites</Link>
+                </li>                                
+            </ul>
+        </nav>
+    </header>
+}
+
+export default MainNavigation;
