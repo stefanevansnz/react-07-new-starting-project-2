3f0058d998f702d5f8384dd1ef042596f315d94a
Stefan Evans <stefan.evans@consegna.cloud>
2022-05-14 16:35:09 +1200

    use children with props

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-07-new-starting-project-2
+
+npm start
--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavouritesPage from './pages/Favourites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
-  return <div>
-    <MainNavigation />
-    <Switch>
-      <Route path='/' exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupsPage />
-      </Route>
-      <Route path='/favourites'>
-        <FavouritesPage />
-      </Route>
-    </Switch>    
-  </div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favourites'>
+          <FavouritesPage />
+        </Route>
+      </Switch> 
+    </Layout>       
+  );
 }
 
 export default App;
