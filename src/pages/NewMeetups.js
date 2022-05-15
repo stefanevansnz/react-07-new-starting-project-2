d0f241dd88c04138713ea6f875bbbbfc9c95aee4
Stefan Evans <stefan.evans@consegna.cloud>
2022-05-15 17:12:53 +1200

    added a form

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,10 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupsPage() {
-    return <div>New Meetup Page</div>
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm/>
+    </section>        
 }
 
 export default NewMeetupsPage;
