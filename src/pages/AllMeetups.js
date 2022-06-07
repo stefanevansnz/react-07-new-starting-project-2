3f70124351975483a8db0330a56629ff6e445e45
Stefan Evans <stefan.evans@consegna.cloud>
2022-06-07 21:48:13 +1200

    added look up

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,49 @@
+import { useState } from 'react'; 
+import { useEffect } from 'react';
+
 import MeetupList from '../components/meetups/MeetupList'
 
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-  ];
 
 function AllMeetupsPage() {
     const { REACT_APP_API_URL } = process.env;
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+      setIsLoading(true);
+      fetch(
+        REACT_APP_API_URL
+      ).then((response) => {
+        return response.json();
+      }).then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id:key,
+            ...data[key]
+          }
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });  
+    }, [REACT_APP_API_URL]);
+
+
+    if (isLoading) {
+      return (
+        <section>
+          <p>Loading...</p>
+        </section>
+      );
+    }
+
     return <section>
         <h1>All Meetup Page</h1>
         <h3>{REACT_APP_API_URL}</h3>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={loadedMeetups} />
     </section>
     
 }
